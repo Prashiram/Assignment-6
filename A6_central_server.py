f4784d469b909edc4964a3e13d8d7de8ce2d5b9b
Prashiram <prashiram99@yahoo.co.in>
2019-11-30 20:50:28 +0530

    added code to central server

--- A6_central_server.py
+++ A6_central_server.py
@@ -1,4 +1,48 @@
 import socket
 import random
+import pickle
+from datetime import datetime
+import datetime
 
-# requests = file.open("requests.txt", "w")
+IP = "127.0.0.1"
+PORT = 5006
+
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind((IP,PORT))
+serversocket.listen(100)
+
+print("Central server is running!")
+print("IP address -"+str(IP)+", Port - "+ str(PORT))
+
+
+requests = file.open("requests.txt", "w")
+
+(s1socket,address1) = serversocket.accept()
+(s2socket,address2) = serversocket.accept()
+(s3socket,address3) = serversocket.accept()
+
+print("Connected to ", address1, ", ", address2, "and ", address3)
+
+while(1):
+	req = []
+	time1 = datetime.now()
+	while(1):
+		if(data == (s1socket.recv(1024) or s2socket.recv(1024) or s3socket.recv(1024))):
+			data.decode()
+			req.append(pickle.loads(data)) #creating a list of requests
+		time2 = datetime.now()
+		if(time2.minute()>time1.minute()+1): 
+			break 
+	time1 = datetime.now()
+	while(1):
+		for r in req:
+			t = listofele[2:]
+			datetime.datetime(t[0],t[1],t[2],t[3], t[4], t[5], t[6])
+			
+		time2 = datetime.now()
+		if(time2.minute()>time1.minute()+1): 
+			break 
+
+
+clientsocket.close()
+serversocket.close()
--- client.py
+++ client.py
@@ -1,10 +1,11 @@
 import socket
 import pickle
+from datetime import datetime
 
 clientsocket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-IP=(input('Enter the IP Address of the server: ')).rstrip()
-PORT=(input('Enter the Port of the server: ')).strip()
-PORT=int(PORT)
+IP= "127.0.0.1"#(input('Enter the IP Address of the server: ')).rstrip()
+# PORT=(input('Enter the Port of the server: ')).strip()
+PORT= #int(PORT)
 clientsocket.connect((IP, PORT))
 
 listofelements=[]
@@ -13,9 +14,16 @@ data=clientsocket.recv(1024)										#recieving list
 listofelements=pickle.loads(data)
 print(listofelements)
 i,j=input('Choose indices i and j you want to swap')
+timestamp = datetime.now() #timestamp
 indices.append(i)
-indices.append(",")
 indices.append(j)
-indices_string= pickle.dumps(indices)							#sending indices
+indices.append(timestamp.year)
+indices.append(timestamp.month)
+indices.append(timestamp.day)
+indices.append(timestamp.hour)
+indices.append(timestamp.minute)
+indices.append(timestamp.second)
+indices.append(timestamp.microsecond)
+indices_string= pickle.dumps(indices)							#sending indices and timestamp
 clientsocket.send(indices)
 clientsocket.close()
