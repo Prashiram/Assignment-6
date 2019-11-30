d795901e11ab464c3ef9989f4786e8e5a6a1ca6c
Prashiram <prashiram99@yahoo.co.in>
2019-11-30 19:28:50 +0530

    three servers

--- a6_server.py
+++ a6_server.py
@@ -1,22 +1,19 @@
 import socket
 import random
+import pickle
+
+s1socket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+s1socket.connect(("127.0.0.1", 5005)) # connect to central server //change port
 
 IP = "127.0.0.1"
 PORT = 5005
 
-# create an INET, STREAMing socket
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# AF_INET indicates IPV4, SOCK_STREAM indicates that the protocol used is TCP 
-
-# bind the socket to IP address and a port
 serversocket.bind((IP,PORT))
-
-# become a server socket
 serversocket.listen(100)
 
-
 listdirectory = file.open("lisdir.txt", "r")
-requests = file.open("requests.txt", "w")
+
 lisdir = []
 line = listdirectory.readline()
 
@@ -28,18 +25,10 @@ print("IP address -"+str(IP)+", Port - "+ str(PORT))
 while(1):
 	(clientsocket,address) = serversocket.accept()
 	print("Got a connection from ", address)
-
-	for num in lisdir:
-		clientsocket.send(bytes(str(num), 'utf8'))
-
-	# clientsocket.send(data.encode()) #send query to client
-	swaptuple = clientsocket.recv(1024).decode() # receive email id
-	tup = swaptuple.split(",")
-
-	requests.write(swaptuple)
-	# temp = lisdir[i]
-	# lisdir[j] = lisdir[i]
-	# lisdir[i] = temp
+	num_list = pickle.dumps(lisdir)
+	clientsocket.send(num_list)
+	swaptuple = clientsocket.recv(1024).decode() # receive i,j tuple
+	s1socket.send(swaptuple.encode()) 
 
 	
 clientsocket.close()
