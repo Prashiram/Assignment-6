de341a193c7e6441de5e268b85dc1d778c3f6ad4
Prashiram <prashiram99@yahoo.co.in>
2019-12-02 15:34:10 +0530

    a6_server check

--- a6_server.py
+++ a6_server.py
@@ -2,15 +2,29 @@ import socket
 import random
 import pickle
 
-s1socket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-s1socket.connect(("127.0.0.1", 5006)) # connect to central server //change port
+from _thread import *
+import threading 
+lock = threading.Lock() 
 
-IP = localhost								#rachit said let Server IP always be local host- automatically takes running systems IP
+def sendtoclient(clientsocket):
+	num_list = pickle.dumps(lisdir)
+	clientsocket.send(num_list)
+	swaptuple = clientsocket.recv(1024).decode() # receive i,j tuple
+	st = pickle.load(swaptuple)
+	t = datetime.datetime(st[2],st[3],st[4],st[5],st[6],st[7],st[8])
+	print("received!")
+	print(st[0],st[1],t)
+	# clientsocket.send(swaptuple.encode()) 
+
+# s1socket= socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+# s1socket.connect(("127.0.0.1", 5006)) # connect to central server //change port
+
+IP = "localhost"
 PORT = 5005
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversocket.bind((IP,PORT))
-serversocket.listen(100)
+serversocket.listen(10)
 
 listdirectory = open("lisdir.txt", "r")
 lisdir = []
@@ -22,11 +36,10 @@ print("IP address -"+str(IP)+", Port - "+ str(PORT))
 
 while(1):
 	(clientsocket,address) = serversocket.accept()
+	lock.acquire()
 	print("Got a connection from ", address)
-	num_list = pickle.dumps(lisdir)
-	clientsocket.send(num_list)
-	swaptuple = clientsocket.recv(1024).decode() # receive i,j tuple
-	s1socket.send(swaptuple.encode()) 
-	
-	clientsocket.close()		#client terminates after one request so it should be terminated inside the loop right?
+	start_new_thread(sendtoclient, (clientsocket,)) 
+
+		
+clientsocket.close()		#client terminates after one request so it should be terminated inside the loop right?
 serversocket.close()
