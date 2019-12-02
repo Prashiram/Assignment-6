049a51252aa893a4f8f1a853d61a4b3e58bb6bf2
Prashiram <prashiram99@yahoo.co.in>
2019-12-03 00:26:31 +0530

    mostly working

--- A6_central_server.py
+++ A6_central_server.py
@@ -1,8 +1,9 @@
 import socket
 import random
 import pickle
+import concurrent.futures
 from _thread import *
-import threading 
+import threading
 lock = threading.Lock() 
 from datetime import datetime
 import datetime
@@ -14,13 +15,14 @@ flag = 0
 requests = open("requests.txt", "w")
 
 IP = "localhost" 
-PORT = 5007
+PORT = 5002
 
 num_list = [1,2,3,4,5,6,7,8,9,10]
 
 centralsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 centralsocket.bind((IP,PORT))
 centralsocket.listen(100)
+centralsocket.settimeout(5)
 
 print("Central server is running!")
 print("IP address -" + str(IP)+ ", Port - " + str(PORT))
@@ -29,62 +31,65 @@ def acceptrequest(serversocket):
 	data = serversocket.recv(1024)
 	# data.decode()
 	req.append(pickle.loads(data)) # creating a list of requests
+	print(req)
 	lock.release()
-	# serversocket.close()
 
 while(1):
-	print("hi from here")
+	flag = 0
 	while(1):
-		req = req[p:]
-
-		t1 = datetime.datetime.now()
-		(serversocket,address) = centralsocket.accept() # issue
-		print("hi from here2")
+		print("hi!")
 		if(flag==0):
-			time1 = datetime.datetime.now()
-			flag = -1
+				time1 = datetime.datetime.now()
+				flag = -1
 
-		lock.acquire()
-		print("Got a connection from ", address)
-		start_new_thread(acceptrequest, (serversocket,))
+		try:
+			(serversocket,address) = centralsocket.accept()
+			lock.acquire()
+			print("hi from here2")
+			print("Got a connection from ", address)
+			start_new_thread(acceptrequest, (serversocket,))
+
+		except socket.timeout:
+			print("hi from here3")
 
 		time2 = datetime.datetime.now()
-		if(time2.second>time1.second+1): 
+		if(time2.second>time1.second+10): 
+			break
+	
+	print("we outta here")
+	time3 = datetime.datetime.now()
+	while(1):
+		final_list=[]
+		temp_list=[]
+		# indices = []
+		if(len(req)!=0):
+
+			for r in req:
+				tstamp=(datetime.datetime(r[2],r[3],r[4],r[4], r[5], r[6], r[7]))
+				temp_list.append(r[0])
+				temp_list.append(r[1])
+				temp_list.append(tstamp)
+				final_list.append(temp_list)
+				temp_list=[]
+
+			final_list.sort(key=itemgetter(2))
+			for f in final_list:
+				i = int(f[0])
+				j = int(f[1])
+				temp = num_list[i]
+				num_list[i] = num_list[j]
+				num_list[j] = temp
+				p=p+1
+			req = req[p:]
+# print('Swapped : ',i,j)
+
+		time4 = datetime.datetime.now()
+		# print(time4)
+		if(time4.second>time3.second+5): 
+
+			print("we're done here")
 			break
 
-
-	# while(1):
-	lock.acquire()
-	time1 = datetime.datetime.now()
-	final_list=[]
-	temp_list=[]
-	# indices = []
-	for r in req:
-		tstamp=(datetime.datetime(r[2],r[3],r[4],r[4], r[5], r[6], r[7]))
-		temp_list.append(r[0])
-		temp_list.append(r[1])
-		temp_list.append(tstamp)
-		final_list.append(temp_list)
-		temp_list=[]
-
-	final_list.sort(key=itemgetter(2))
-	for f in final_list:
-		i = int(f[0])
-		j = int(f[1])
-		temp = num_list[i]
-		num_list[i] = num_list[j]
-		num_list[j] = temp
-
 	print(num_list)
-		# time2 = datetime.datetime.now()
-		# if(time2.second>time1.second+30): 
-		# 	string = pickle.dumps(num_list)	# sending indices and timestamp
-		# #??????????????	serversocket.send(string) # send to all servers??????????????
-		# 	if(req.index(r)!=(req.len()-1)):
-		# 		p = req.index(r) + 1
-		# 		break
 
-# 	s1socket.close()
-# 	s2socket.close()
-# 	s3socket.close()
 serversocket.close()
